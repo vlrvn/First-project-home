5b7fadc597cdd2272163a5ba727ed940f64e5db8
YANA <yanapos20@gmail.com>
2022-01-25 00:19:43 +0400

    новый проект

---
+++ тест/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace тест
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Номер задачи:");
+            string? name = Console.ReadLine();
+            Console.WriteLine($"build {name}");
+        }
+    }
+}
+
+
